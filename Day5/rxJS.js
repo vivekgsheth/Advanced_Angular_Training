18058b2b6444312fcae178321ee808130e70cd58
Vivek Sheth <vsheth@hhaexchange.com>
2021-03-13 11:38:31 +0530

    Adding files

---
+++ Day5/rxJS.js
@@ -0,0 +1,15 @@
+// source
+let product = {
+    price: 10
+};
+
+dispProduct = () => {
+    return product.price;
+}
+
+let a = dispProduct();
+let b = dispProduct();
+
+console.log(a);
+console.log(b);
+
